9ca1b0f84ad9aa545d673aee7e5037b9637f52d2
pianelson <pianelson@knights.ucf.edu>
2020-11-05 11:43:58 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,10 +13,10 @@ const users = [];
 
 const siteConfig = {
     title: 'Pia Nelson', // Title for your website.
-    tagline: 'Continuously improving, continuously improving.',
+    tagline: 'Continuously improving, continuously learning.',
     profilePhoto: 'img/profile.png',
     url: 'https://pianelson.github.io', // Your website URL
-    baseUrl: '/tech-portfolio/resume', // Base URL for your project */
+    baseUrl: '/tech-portfolio', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
