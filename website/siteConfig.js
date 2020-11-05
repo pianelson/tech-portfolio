b58cfe603cff6e4a4ef8ba7875f56daadb5b1ca5
pianelson <pianelson@knights.ucf.edu>
2020-11-05 04:45:05 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Pia Nelson', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    tagline: 'Continuously improving, continuously improving.',
     profilePhoto: 'img/profile.png',
     url: 'https://pianelson.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
